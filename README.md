c67931845e2c9515cb08bb472446222950597211
Halvard Skogsrud <halvard@skogsrud.com>
2012-09-18 16:45:07 +1000

    Renamed subproject Gradle build scripts
    To make them easier to distinguish in the IDE tabs. Unfortunately
    IntelliJ IDEA now nags about configuring a Groovy SDK which is
    unnecessary.

--- README.md
+++ README.md
@@ -1,10 +1,23 @@
 learnangularjs
 ==============
 
-Experimentation with AngularJS
+An example AngularJS application with a Java backend.
+
+To create IntelliJ IDEA project files, run:
+
+    gradle idea
+
+Ensure you have the following IDEA plugins:
+
+* AngularJS
+* Gradle
+* JSTestDriver
+* All the bundled JavaScript plugins
 
 Suggested reading:
+
 * Dependency Injection: http://docs.angularjs.org/guide/di
 * Unit Testing: http://docs.angularjs.org/guide/dev_guide.unit-testing
 * Internet Explorer Compatibility: http://docs.angularjs.org/guide/ie
 * Data Binding: http://stackoverflow.com/questions/9682092/databinding-in-angularjs/9693933#9693933
+
--- service/src/test/java/com/thoughtworks/learnangularjs/server/TomcatServer.java
+++ service/src/test/java/com/thoughtworks/learnangularjs/server/TomcatServer.java
@@ -4,22 +4,34 @@ import org.apache.catalina.Context;
 import org.apache.catalina.deploy.FilterDef;
 import org.apache.catalina.deploy.FilterMap;
 import org.apache.catalina.startup.Tomcat;
+import org.apache.commons.cli.*;
 
 public class TomcatServer {
-    private static final int PORT = 8000;
+    // These default directories are relative to the root project directory, they are overridden in the Gradle build script
+    private static final String DEFAULT_SERVICE_BASE_DIR = "service/src/main/webapp";
+    private static final String DEFAULT_CLIENT_BASE_DIR = "client/src/main/webapp";
+    private static final String DEFAULT_PORT = "8000";
+
+    private static final String OPTION_SERVICE_BASE_DIR = "serviceBaseDir";
+    private static final String OPTION_CLIENT_BASE_DIR = "clientBaseDir";
+    private static final String OPTION_PORT = "port";
+    private static final String OPTION_HELP = "help";
 
     public static void main(String[] args) throws Exception {
         new TomcatServer().run(args);
     }
 
     public void run(String[] args) throws Exception {
+        CommandLine commandLine = parseCommandLine(args);
         Tomcat tomcat = new Tomcat();
-        tomcat.setPort(PORT);
+        tomcat.setPort(Integer.valueOf(commandLine.getOptionValue("port", DEFAULT_PORT)));
         tomcat.setBaseDir(System.getProperty("java.io.tmpdir"));
         tomcat.getHost().setAppBase(System.getProperty("user.dir"));
 
-        Context applicationContext = tomcat.addWebapp("/service", "src/main/webapp");
-        Context staticFilesContext = tomcat.addWebapp("/app", "../client/src/main/webapp");
+        Context applicationContext = tomcat.addWebapp("/service", commandLine
+                .getOptionValue(OPTION_SERVICE_BASE_DIR, DEFAULT_SERVICE_BASE_DIR));
+        Context staticFilesContext = tomcat.addWebapp("/app", commandLine
+                .getOptionValue(OPTION_CLIENT_BASE_DIR, DEFAULT_CLIENT_BASE_DIR));
 
         ensureNoBrowserCaching(applicationContext);
         ensureNoBrowserCaching(staticFilesContext);
@@ -46,5 +58,40 @@ public class TomcatServer {
         filterDefinition.setFilterClass("org.apache.catalina.filters.ExpiresFilter");
         filterDefinition.addInitParameter("ExpiresDefault", "access plus 0 seconds");
         return filterDefinition;
+    }
+
+    private CommandLine parseCommandLine(String[] args) throws ParseException {
+        Options options = createCommandLineOptions();
+        CommandLine line = null;
+        try {
+            line = new GnuParser().parse(options, args);
+            handleHelpOptionIfPresent(options, line);
+        } catch (UnrecognizedOptionException e) {
+            printHelp(options, e.getMessage());
+            System.exit(1);
+        }
+        return line;
+    }
+
+    private Options createCommandLineOptions() {
+        Options options = new Options();
+        options.addOption("svc", OPTION_SERVICE_BASE_DIR, true,
+                          "Base directory for service web application, defaults to '" + DEFAULT_SERVICE_BASE_DIR + "'");
+        options.addOption("app", OPTION_CLIENT_BASE_DIR, true,
+                          "Base directory for client application, defaults to '" + DEFAULT_CLIENT_BASE_DIR + "'");
+        options.addOption("p", OPTION_PORT, true, "Port number used by the server, defaults to " + DEFAULT_PORT);
+        options.addOption("h", OPTION_HELP, false, "Display this help text");
+        return options;
+    }
+
+    private void handleHelpOptionIfPresent(Options options, CommandLine line) {
+        if (line.hasOption(OPTION_HELP)) {
+            printHelp(options, "");
+            System.exit(0);
+        }
+    }
+
+    private void printHelp(Options options, String prefix) {
+        new HelpFormatter().printHelp(prefix + "java " + getClass().getName(), options);
     }
 }
