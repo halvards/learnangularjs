27709461f9992269a81f4c928615f711ab433d7d
Halvard Skogsrud <halvard@skogsrud.com>
2012-09-24 15:41:45 +1000

    Added integration tests for service
    Only dummy tests for now.

---
+++ service/src/test/java/com/thoughtworks/learnangularjs/DummyUnitTest.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.learnangularjs;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class DummyUnitTest {
+    @Test
+    public void testUnit() throws Exception {
+        assertThat(true, equalTo(true));
+    }
+}
