cfa2d4afeff96680fe08dd3f6f3a8e90bdf68931
Halvard Skogsrud <halvard@skogsrud.com>
2012-09-26 11:49:21 +1000

    Added authentication to stub service
    Added PassportJS for authentication in the stub service

--- stubservice/README.md
+++ stubservice/README.md
@@ -4,6 +4,16 @@ learnangularjs:stubservice
 Stub implementation of the Tasks service (from the 'service' sub-project), to enable fast feedback for development of the AngularJS-based
 browser client (as well as other future clients like native mobile applications).
 
-It uses [NodeJS](http://nodejs.org/) and [Express](http://expressjs.com/) (a Sinatra-like JavaScript framework).
+It uses [NodeJS](http://nodejs.org/) and [Express](http://expressjs.com/) (a Sinatra-like JavaScript framework),
+with [Passport](http://passportjs.org/) for authentication.
 
-The stub service can either return responses based on routes set up in stubservice.js, or from static files in the 'static' directory.
+The stub service can either return responses based on routes set up in stubservice.js, or from static files in the 'public' and
+'secure' directories.
+
+After installing NodeJS, run this command to fetch dependencies:
+
+    npm install
+
+Run the stub service with this command:
+
+    node stubservice
--- stubservice/stubservice.js
+++ stubservice/stubservice.js
@@ -1,22 +1,119 @@
-var express = require('express');  // http://expressjs.com/
+// How to use Passport: https://github.com/jaredhanson/passport-local/blob/master/examples/login/app.js
+
+var express = require('express')
+    , flash = require('connect-flash')
+    , passport = require('passport')
+    , util = require('util')
+    , LocalStrategy = require('passport-local').Strategy;
+
+var users = [
+    { id:1, username:'public', password:'public', roles:[] },
+    { id:2, username:'user',   password:'user',   roles:['ROLE_USER'] },
+    { id:3, username:'admin',  password:'admin',  roles:['ROLE_ADMIN'] }
+];
+
+function findById(id, done) {
+    var index = id - 1;
+    if (users[index]) {
+        done(null, users[index]);
+    } else {
+        done(new Error('User ' + id + ' does not exist'));
+    }
+}
+
+function findByUsername(username, done) {
+    for (var i = 0, length = users.length; i < length; i++) {
+        var user = users[i];
+        if (user.username === username) {
+            return done(null, user);
+        }
+    }
+    return done(null, null);
+}
+
+function ensureAuthenticated(request, response, next) {
+    if (request.isAuthenticated()) {
+        return next();
+    }
+    response.redirect('/service/login/login.html')
+}
+
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+    findById(id, function (error, user) {
+        done(error, user);
+    });
+});
+
+passport.use(new LocalStrategy({
+        usernameField:'j_username',
+        passwordField:'j_password'
+    },
+    function (username, password, done) {
+        findByUsername(username, function (error, user) {
+            if (error) {
+                return done(error);
+            }
+            if (!user) {
+                return done(null, false, { message:'Unknown user ' + username });
+            }
+            if (user.password != password) {
+                return done(null, false, { message:'Invalid password' });
+            }
+            return done(null, user);
+        })
+    }
+));
+
 var app = express();
+app.configure(function () {
+    app.use(express.logger("dev"));
+    app.use(express.cookieParser());
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express.session({ secret:'keyboard cat' }));
+    app.use(flash());
+    app.use(passport.initialize());
+    app.use(passport.session());
+    app.use(app.router);
 
-// Serve up the client JS application
-app.use('/app', express.static('../client/src/main/webapp'));
+    // Serve up the client JS application
+    app.use('/app', express.static('../client/src/main/webapp'));
 
-// Serve static files for simple stubbing
-app.use('/service/public', express.static('public'));
+    // Reuse login pages from real service
+    app.use('/service/login', express.static('../service/src/main/webapp/login'));
+
+    // Serve static files for simple stubbing
+    app.use('/service/public', express.static('public'));
+    app.use('/service/secure', express.static('secure'));
+});
+
+app.post(/.*\/j_security_check/,
+    passport.authenticate('local', { failureRedirect:'/service/login/login-failed.html', failureFlash:true }),
+    function (request, response) {
+        response.redirect('/hello');
+    });
+
+app.get('/service/logout', function (request, response) {
+    request.logout();
+    response.redirect('/service/login/logout.html');
+});
 
 // Hello World
 app.get('/hello', function (request, response) {
     response.set('Content-Type', 'text/plain');
     response.send('Hello World');
 });
+
 app.get('/hellojson', function (request, response) {
     response.send({message:'Hello World'});
 });
-app.get('/hello/:name', function (request, response) {
-    response.send({message:'Hello ' + request.params.name});
+
+app.get('/hello/:message', ensureAuthenticated, function (request, response) {
+    response.send({message:'Hello ' + request.user.username + ' ' + request.params.message});
 });
 
 // This section must appear last
