57c3a02096e0d1a79da4b55dcb98b53243ec37c1
Halvard Skogsrud <halvard@skogsrud.com>
2012-10-02 14:01:00 +1000

    Introduced TinyType to service code
    Read more about the Tiny Types pattern here:
    http://darrenhobbs.com/2007/04/11/tiny-types/

--- service/src/main/java/com/thoughtworks/learnangularjs/domain/Amount.java
+++ service/src/main/java/com/thoughtworks/learnangularjs/domain/Amount.java
@@ -11,44 +11,14 @@ import java.math.RoundingMode;
 
 @JsonSerialize(using = AmountSerializer.class)
 @JsonDeserialize(using = AmountDeserializer.class)
-public class Amount implements Comparable<Amount> {
+public class Amount extends TinyType<BigDecimal> {
     private static final int SCALE = 2;
-
-    private final BigDecimal amount;
 
     public Amount(String amount) {
         this(new BigDecimal(Preconditions.checkNotNull(amount)));
     }
 
     public Amount(BigDecimal amount) {
-        this.amount = amount.setScale(SCALE, RoundingMode.HALF_UP);
-    }
-
-    @Override
-    public boolean equals(Object other) {
-        if (this == other) return true;
-        if (other == null || getClass() != other.getClass()) {
-            return false;
-        }
-        Amount that = (Amount) other;
-        if (!this.amount.equals(that.amount)) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        return amount.hashCode();
-    }
-
-    @Override
-    public String toString() {
-        return amount.toString();
-    }
-
-    @Override
-    public int compareTo(Amount that) {
-        return this.amount.compareTo(that.amount);
+        super(amount.setScale(SCALE, RoundingMode.HALF_UP));
     }
 }
---
+++ service/src/main/java/com/thoughtworks/learnangularjs/domain/TinyType.java
@@ -0,0 +1,55 @@
+package com.thoughtworks.learnangularjs.domain;
+
+import com.google.common.base.Preconditions;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+
+import java.io.Serializable;
+
+public abstract class TinyType<T extends Comparable & Serializable> implements Comparable<TinyType<T>>, Serializable {
+
+    private final T value;
+
+    public TinyType(T value) {
+        this.value = Preconditions.checkNotNull(value);
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return value == null ? "" : value.toString();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+        if (other == null) {
+            return false;
+        }
+        if (!(other.getClass().equals(this.getClass()))) {
+            return false;
+        }
+        TinyType that = (TinyType) other;
+        return new EqualsBuilder().append(this.getValue(), that.getValue()).isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().append(getValue()).toHashCode();
+    }
+
+    @Override
+    public int compareTo(TinyType<T> that) {
+        if (this.getClass() != that.getClass()) {
+            throw new IllegalArgumentException(String.format("Trying to compare instance of [%s] with instance of [%s]",
+                                                             this.getClass().getSimpleName(),
+                                                             that.getClass().getSimpleName()));
+        }
+        return this.value.compareTo(that.value);
+    }
+}
